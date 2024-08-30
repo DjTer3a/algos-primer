e408c7fcb7e6b4b31ab838457e33c5fcb32bd7d8
DjTer3a <mostafa@higazy.dev>
2024-08-31 02:06:47 +0400

    match brackets for csprimer in js

---
+++ bracketsmatch.js
@@ -0,0 +1,45 @@
+const assert = require("assert");
+
+function isBalanced(brackets) {
+  const stack = [];
+  const chars = { "{": "}", "[": "]", "(": ")" };
+  const keys = Object.keys(chars);
+  const values = Object.values(chars);
+  for (let i = 0; i < brackets.length; i++) {
+    let char = brackets[i];
+    if (keys.includes(char)) {
+      stack.push(char);
+    } else if (values.includes(char)) {
+      let popped = stack.pop();
+      if (popped === undefined) {
+        console.log("No more parantheses to close issue at pos", i);
+        return false;
+      } else if (keys.includes(popped)) {
+        if (char === chars[popped]) {
+          continue;
+        } else {
+          console.log(
+            "invalid char:",
+            char,
+            "at pos",
+            i,
+            "expected",
+            chars[popped]
+          );
+          return false;
+        }
+      } else {
+        console.log("invalid char: ", popped, "at pos:", i);
+        return false;
+      }
+    } else {
+      continue;
+    }
+  }
+  return true;
+}
+assert.ok(isBalanced("{]") === false);
+assert.ok(isBalanced("[{()(())}]") === true);
+assert.ok(isBalanced("[{()( 99 , ! # @ (0))}]") === true);
+assert.ok(isBalanced("[{(())(())})))]") === false);
+assert.ok(isBalanced("[{()]}") === false);
